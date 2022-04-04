c12f30d94af3724ec328df90e586deb9e1b11e91
kevin <kevinli8831@gmail.com>
2022-04-05 04:13:30 +0800

    create first component and work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from "./Components/NavBar";
 function App() {
   return (
     <div className="App">
+      <NavBar>
+
+      </NavBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
